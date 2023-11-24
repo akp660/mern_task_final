ab7add5c06caf84eb2c09f56b9f57b731e7edc0e
abhijeet <abhijeetpandey.dhn@gmail.com>
2023-11-24 13:27:38 +0530

    Co-authored-by: Jishnu Chowdhury <jihacshnu@users.noreply.github.com>

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const connectDb = require("./config/db");
+const path = require("path");
 
 dotenv.config();
 mongoose.set("strictQuery", false);
@@ -16,7 +17,20 @@ const tasRouter = require("./routes/tasks");
 app.use("/api/tasks",tasRouter)
 
 
+const __dirname1 = path.resolve();
+  if(process.env.NODE_ENV === "production") {
+      app.use(express.static(path.join(__dirname1, "/frontend/task-manager-app/build")));
+      
+      app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname1, "frontend", "task-manager-app","build", "index.html"));
+});
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is Running Successfully");
+  });
+  }
 
 
-const PORT = process.env.PORT || 5000;
+
+const PORT = process.env.PORT || 3002;
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
